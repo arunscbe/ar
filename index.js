d0cd294fc3bbe24a62183a9d105bab21937a9130
Arun kumar <arunscbe@gmail.com>
2023-01-20 12:46:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function backToPage5() {
 
 function changeModel(name) {
   const modelViewer = document.querySelector("model-viewer");
-
+alert('kkk');
   const base = "./models/" + name;
   modelViewer.src = base + ".gltf";
    modelViewer.iosSrc = base + ".usdz";
