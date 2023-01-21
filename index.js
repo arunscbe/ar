457a48f925c002d104f5b0caab934d32b2f455cb
Arun kumar <arunscbe@gmail.com>
2023-01-21 20:26:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function backToPage5() {
 }
 
 function changeModel(name) {
-  alert("TWO");
+  alert("THREE");
   const modelViewer = document.getElementById("Cigar");
 
   const base = "./models/" + name;
