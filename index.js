d8f214ee9f28fcd758a84ee6a2e081f7739b317c
Arun kumar <arunscbe@gmail.com>
2023-01-22 14:23:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+ alert("Seven");
 
 // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
 let vh = window.innerHeight * 0.01;
@@ -95,7 +95,7 @@ function backToPage5() {
 }
 
 function changeModel(name) {
-  alert("Seven");
+ 
   const modelViewer = document.getElementById("Cigar");
 
   const base = "./models/" + name;
