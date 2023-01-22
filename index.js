c4c986ee7fea37fe4008347223d67008ce3e65ad
Arun kumar <arunscbe@gmail.com>
2023-01-22 15:28:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function changeModel(name) {
   const base = "./models/" + name;
    modelViewer.src = base + ".gltf";  
 //    modelViewer.setAttribute("ios-src", base + ".usdz");
-//    modelViewer.setAttribute("autoplay", true);
+    modelViewer.setAttribute("autoplay", true);
     modelViewer.iosSrc === base + ".usdz";
  console.log(modelViewer);
 
